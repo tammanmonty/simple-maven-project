9c96c96a14b460432a4397085dec1695ae979d75
Tamman <tamman.montanaro@fdmgroup.com>
2025-10-19 22:03:29 -0600

    first commit

---
+++ src/main/java/montanaro/tamman/com/example/demo/HelloWorldApplication.java
@@ -0,0 +1,19 @@
+package montanaro.tamman.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/montanaro/tamman/com/example/demo/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package montanaro.tamman.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
